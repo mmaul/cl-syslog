2fa4323fb5bcf645782b92ea2a649ca6a97afbbf
Robert Smith <robert@rigetti.com>
2018-12-13 17:13:55 -0800

    valid ip4 and ip6 testing

--- cl-syslog.asd
+++ cl-syslog.asd
@@ -10,7 +10,7 @@
   :license "MIT (See LICENSE)"
   :version (:read-file-form "VERSION.txt")
   :description "Local-only syslog logging."
-  :depends-on (#:alexandria #:cffi #:global-vars #:usocket)
+  :depends-on (#:alexandria #:cffi #:global-vars #:usocket #:split-sequence)
   :serial t
   :components ((:file "package")
                (:file "variable")
--- rfc5424-reserved.lisp
+++ rfc5424-reserved.lisp
@@ -11,10 +11,33 @@
               :validator (lambda (x) (member x '("0" "1") :test #'string=)))
   (|syncAccuracy| :validator (lambda (x) (every #'digit-char-p x))))
 
+(defun ip4-address-p (x)
+  (flet ((valid-group-p (x)
+           (and (<= 1 (length x) 3)
+                (every #'digit-char-p x)
+                (or (string= "0" x)
+                    (not (char= #\0 (char x 0))))
+                (<= 0 (parse-integer x) 255))))
+    (and (stringp x)
+         (= 3 (count #\. x))
+         (every #'valid-group-p (split-sequence:split-sequence #\. x)))))
+
+(defun ip6-address-p (x)
+  (labels ((hex-digit-char-p (c)
+             (or (digit-char-p c)
+                 (find c "aAbBcCdDeEfF")))
+           (valid-group-p (x)
+             (or (zerop (length x))
+                 (and (= 4 (length x))
+                      (every #'hex-digit-char-p x)
+                      (<= 0 (parse-integer x :radix 16) #xFFFF)))))
+    (and (stringp x)
+         (= 7 (count #\: x))
+         (every #'valid-group-p (split-sequence:split-sequence #\: x)))))
+
 (defun ip-address-p (x)
-  (declare (ignore x))
-  ;; TODO: actually parse an IP address
-  t)
+  (or (ip4-address-p x)
+      (ip6-address-p x)))
 
 (define-structured-data-id |origin| (:standard t)
   (|ip| :allow-repetitions t
